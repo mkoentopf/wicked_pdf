b00a3af43e43716cfbf9bc99f270db5f20fb9d07
mkoentopf <matthias@koentopf.de>
2020-11-19 23:07:00 +0100

    Disable asset version in railtie for WM

--- lib/wicked_pdf/railtie.rb
+++ lib/wicked_pdf/railtie.rb
@@ -8,7 +8,7 @@ class WickedPdf
       initializer 'wicked_pdf.register', :after => 'remotipart.controller_helper' do |_app|
         ActiveSupport.on_load(:action_controller) do
           ActionController::Base.send :prepend, PdfHelper
-          ActionView::Base.send :include, WickedPdfHelper::Assets
+          ActionView::Base.send :include, WickedPdfHelper # ::Assets
         end
       end
     end
